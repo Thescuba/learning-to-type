e27177829b70b6bd6db4ffe22a55187f243394f0
Steve Tang <Stevetang92@gmail.com>
2018-10-19 16:22:38 -0700

    Declare and init char_list

--- LogParser.py
+++ LogParser.py
@@ -4,12 +4,24 @@ from pathlib import Path
 filename = input("Please enter file name (without .txt extension): ")
 file_path = Path("logs/" + filename + ".txt")
 
+relevant_keys = {'Key.space': ' ', 'Key.backspace': 'BS'}
+
 # Read file contents into contents
 file_stream = open(file_path, "r")
+char_list = []
 
 if file_stream.mode == "r":
     contents = file_stream.readlines()
     file_stream.close()
 
-for i in contents:
-    print(i.split(':'))
+for line in contents:
+    key = line[25:-1]
+    if len(key) == 3:
+        #print(key)
+        char_list.append(key[1])
+    elif key in relevant_keys:
+        char_list.append(relevant_keys[key])
+print(char_list)
+print(''.join(char_list))
+    
+#
