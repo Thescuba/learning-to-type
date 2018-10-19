660e223c990d2b7ea746c98db88b216bae52cedd
Dan T <dantu22@live.com>
2018-10-19 15:17:08 -0700

    Read file line by line

--- LogParser.py
+++ LogParser.py
@@ -4,10 +4,12 @@ from pathlib import Path
 filename = input("Please enter file name (without .txt extension): ")
 file_path = Path("logs/" + filename + ".txt")
 
-# Read file contents
+# Read file contents into contents
 file_stream = open(file_path, "r")
 
 if file_stream.mode == "r":
-    contents = file_stream.read()
+    contents = file_stream.readlines()
+    file_stream.close()
 
-print(contents)
+for i in contents:
+    print(i)
